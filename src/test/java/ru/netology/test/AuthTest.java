a0d3a3d9d9cb842e0a82ae3d6ede2cd494910ae9
AlexeyVFrolov <isbase@mail.ru>
2021-05-30 17:27:27 +0300

    Code corrected

--- src/test/java/ru/netology/test/AuthTest.java
+++ src/test/java/ru/netology/test/AuthTest.java
@@ -5,7 +5,6 @@ import lombok.val;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import ru.netology.data.DataGenerator;
 
 import java.time.Duration;
 
@@ -13,9 +12,9 @@ import java.time.Duration;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static ru.netology.data.DataGenerator.Registration.getRegisteredUser;
-import static ru.netology.data.DataGenerator.getRandomLogin;
-import static ru.netology.data.DataGenerator.getRandomPassword;
+import static ru.netology.test.DataGenerator.Registration.getRegisteredUser;
+import static ru.netology.test.DataGenerator.getRandomLogin;
+import static ru.netology.test.DataGenerator.getRandomPassword;
 
 class AuthTest {
 
@@ -28,9 +27,7 @@ class AuthTest {
     @DisplayName("Should successfully login with active registered user")
     void shouldSuccessfulLoginIfRegisteredActiveUser() {
         val registeredUser = DataGenerator.Registration.getRegisteredUser("active");
-        // TODO: добавить логику теста, в рамках которого будет выполнена попытка входа в личный кабинет с учётными
-        //  данными зарегистрированного активного пользователя, для заполнения полей формы используйте
-        //  пользователя registeredUser
+
         $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
         $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
         $(".button").click();
@@ -41,8 +38,7 @@ class AuthTest {
     @DisplayName("Should get error message if login with blocked registered user")
     void shouldGetErrorIfBlockedUser() {
         val blockedUser = getRegisteredUser("blocked");
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет,
-        //  заблокированного пользователя, для заполнения полей формы используйте пользователя blockedUser
+
         $("[data-test-id='login'] input").setValue(blockedUser.getLogin());
         $("[data-test-id='password'] input").setValue(blockedUser.getPassword());
         $(".button").click();
@@ -54,9 +50,7 @@ class AuthTest {
     void shouldGetErrorIfWrongLogin() {
         val registeredUser = getRegisteredUser("active");
         val wrongLogin = getRandomLogin();
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
-        //  логином, для заполнения поля формы "Логин" используйте переменную wrongLogin,
-        //  "Пароль" - пользователя registeredUser
+
         $("[data-test-id='login'] input").setValue(wrongLogin);
         $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
         $(".button").click();
@@ -68,9 +62,7 @@ class AuthTest {
     void shouldGetErrorIfWrongPassword() {
         val registeredUser = getRegisteredUser("active");
         val wrongPassword = getRandomPassword();
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
-        //  паролем, для заполнения поля формы "Логин" используйте пользователя registeredUser,
-        //  "Пароль" - переменную wrongPassword
+
         $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
         $("[data-test-id='password'] input").setValue(wrongPassword);
         $(".button").click();
